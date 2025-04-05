7e0486ad1c48efca697a96256e053f1b5f2abbe6
Platonich123 <145435938+Platonich123@users.noreply.github.com>
2025-04-05 11:12:51 +0300

    Add files via upload

---
+++ dev/architercture/lab1/backend/mainapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from django.conf import settings
+
+import rest_framework
+from rest_framework import viewsets
+
+from .models import Monowheel
+from .serializers import MonowheelSeriallizer
+
+class MonowheelViewSet(viewsets.ModelViewSet):
+    queryset = Monowheel.objects.all()
+    serializer_class = MonowheelSeriallizer
